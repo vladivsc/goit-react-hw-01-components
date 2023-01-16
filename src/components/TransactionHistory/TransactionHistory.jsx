0b288b5e606c0b871f6b24fd229cd2c27a11e9e9
Vladyslav <101827554+vladivsc@users.noreply.github.com>
2023-01-16 20:10:00 +0100

    Update TransactionHistory.jsx

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -26,3 +26,12 @@ const TransactionHistory = ({ items }) => {
 };
 
 export default TransactionHistory;
+
+TransactionHistory.propTypes = {
+  items:  PropTypes.arrayOf(PropTypes.shape({
+    id:PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    amount: PropTypes.string.isRequired,
+    currency: PropTypes.string.isRequired
+  }))
+}
